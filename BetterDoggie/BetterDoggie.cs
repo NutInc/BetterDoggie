a8e40d62c6dc75c2d0cb9ae8d53b054c0c6c01d0
Parkeymon <pwolfe@stealthturtle.com>
2021-12-11 23:33:57 -0800

    version bump

--- BetterDoggie/BetterDoggie.cs
+++ BetterDoggie/BetterDoggie.cs
@@ -14,7 +14,7 @@
         public override string Author => "Parkeymon";
         public override string Name => "BetterDoggie";
         public override string Prefix => "better_doggie";
-        public override Version Version => new Version(1, 2, 0);
+        public override Version Version => new Version(1, 2, 1);
         public override Version RequiredExiledVersion => new Version(4, 0, 0);
         public override PluginPriority Priority => PluginPriority.Low;
         
